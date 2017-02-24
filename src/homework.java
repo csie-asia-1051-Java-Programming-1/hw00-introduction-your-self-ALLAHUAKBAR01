15fdb4a4226ca2a3a4331c824b2c1c96dadac8e1
ALLAHUAKBAR01 <105021019@live.asia.edu.tw>
2017-02-24 15:02:37 +0800

    homework 0224
    2017 02 24

---
+++ src/homework.java
@@ -0,0 +1,24 @@
+/*
+ * Topic:第一次作業繳交
+ *       自我介紹
+ * Date:2017/02/24 
+ * Author:105021019 林承鴻
+ */
+
+
+
+public class homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("資工系資電組");
+		System.out.println("105021019");
+		System.out.println("林承鴻");
+		
+		
+	
+		
+}
+
+}
